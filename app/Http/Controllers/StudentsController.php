52e790b3432bd1c2173578058ee6bc787030b478
Mr-MistySmile <grisha.lalayan@mail.ru>
2019-07-04 00:00:45 +0400

    добавил студентов

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Students;
 
 class StudentsController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $students = Students::all();
+        //dd($students);
+        return view('students', compact('students'));
+    }
 }
---
+++ resources/views/students.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <table class= "table table-sm">
+        <thead>
+            <th> Имя </th>
+            <th> День рождения</th>
+            <th> Фото </th>
+            <th> id</th>
+
+        </thead>
+        <tbody>
+            <tr>
+                @foreach ($students as $student)
+                 <tr>
+                       // <td><div class="body">{{$student->name}} </div> </td>
+                        <td><div class="body">{{$student->birthday}} </div> </td>
+                        <td><div class="body">{{$student->img}} </div> </td>
+                        <td><div class="body">{{$student->id}} </div> </td>
+
+                </tr>
+                @endforeach
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
