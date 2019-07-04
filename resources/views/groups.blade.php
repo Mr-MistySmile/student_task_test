277eaec6281e3088ab8b80b6879c990b3899f7ef
Mr-MistySmile <grisha.lalayan@mail.ru>
2019-07-05 00:38:50 +0400

    Изменение html

--- resources/views/groups.blade.php
+++ resources/views/groups.blade.php
@@ -7,11 +7,15 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+<style>
+        body {background: #1eb3b0;
+            color: #fc0; }
+</style>
 <body>
-    <table class= "table table-sm">
+    <table class= "table table-sm table-dark">
         <thead>
             <th> Название </th>
-            <th> Описание</th> 
+            <th> Описание</th>
             <th> Оценка по матеше </th>
             <th> Оценка по русскому</th>
             <th> Оценка по английскому</th>
--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -7,8 +7,12 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+<style>
+        body {background: #1eb3b0;
+            color: #fc0; }
+</style>
 <body>
-    <table border="3">
+    <table class= "table table-sm table-primary">
         <thead>
             <th> Имя </th>
             <th> День рождения</th>
