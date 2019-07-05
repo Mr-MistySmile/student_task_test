d9ac0ba65d02f56c773342212bb0fb64fbf5e415
Mr-MistySmile <grisha.lalayan@mail.ru>
2019-07-05 12:44:21 +0400

    Финальная версия1.4

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -34,8 +34,8 @@
                       <input type="text" value="{{$group->average_eng}}" placeholder="{{$group->average_eng}}" name="average_eng">
                     </div>
             <div class="input-group-prepend">
-              <span class="input-group-text">Принять изменения</span>
-              <button class="btn btn-outline-info" type="submit">~</button></div>
+             
+              <button class="btn btn-outline-info" type="submit">Принять изменения</button></div>
             </div>
           </td>
         </form>
